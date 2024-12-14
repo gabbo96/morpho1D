5e967913be6a9b3381af77a7a7dfb2777bcb9d26
gabbo96 <gabriele.barile96@gmail.com>
2024-12-14 16:10:20 +0100

    Fixed critical depth bug, config files

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
+import os
 import json
 import matplotlib.pyplot as plt
 from module import *
+
+inputs_dir = "inputs_files"
+inputs_filename = "slope_decrease"
 
 bed_color = "goldenrod"
 water_color = "dodgerblue"
 
 # Import input dictionary
-with open("inputs.json", "r") as f:
+with open(os.path.join(inputs_dir, f"{inputs_filename}.json"), "r") as f:
     in_d = json.load(f)
 Q = in_d["Q"]
 C = in_d["C"]
@@ -59,7 +63,7 @@ for n in range(in_d["num_iter"]):
         Y_sub[-1] = Y_cr[-1]
     H_sub[-1] = energy(Y_sub[-1], Q, B[-1], eta[-1])
 
-    # Spatial loop
+    # Spatial loop in upstream direction
     for i in range(M - 2, -1, -1):
         dx = x[i + 1] - x[i]
         j = energy_slope(Q, B[i + 1], C, Y_sub[i + 1])
@@ -67,7 +71,8 @@ for n in range(in_d["num_iter"]):
 
         # Check if the specific energy is lower than the minimum for the given flow discharge
         if H_sub[i] - eta[i] < E_min[i]:
-            Y_sub[i] = Y_cr[i]
+            Y_sub[i] = Y_cr[i] + 0.05
+            H_sub[i] = energy(Y_sub[i], Q, B[i], eta[i])
         else:
             YL = Y_cr[i]
             YR = 10 * Y_cr[i]
@@ -80,14 +85,15 @@ for n in range(in_d["num_iter"]):
         Y_super[0] = Y_cr[0]
     H_super[0] = energy(Y_super[0], Q, B[0], eta[0])
 
-    # Spatial loop
+    # Spatial loop in downstream direction
     for i in range(M - 1):
         dx = x[i + 1] - x[i]
         j = energy_slope(Q, B[i], C, Y_super[i])
         H_super[i + 1] = H_super[i] - j * dx
         # Check if the specific energy is lower than the minimum for the given flow discharge
         if H_super[i + 1] - eta[i + 1] < E_min[i + 1]:
-            Y_super[i + 1] = Y_cr[i + 1]
+            Y_super[i + 1] = Y_cr[i + 1] - 0.05
+            H_super[i + 1] = energy(Y_super[i + 1], Q, B[i + 1], eta[i + 1])
         else:
             YL = 1e-6
             YR = Y_cr[i + 1]
@@ -109,9 +115,9 @@ for i in range(len(Y) - 1):
 
 # ----------------------------------- Plots ---------------------------------- #
 plt.plot(x, eta, color=bed_color, label="Bed elevation")
-# plt.plot(x, eta + Y, color=water_color, label="Water level")
-plt.plot(x, eta + Y_sub, label="sub")
-plt.plot(x, eta + Y_super, label="super")
+plt.plot(x, eta + Y, color=water_color, label="Water level")
+# plt.plot(x, eta + Y_sub, label="sub")
+# plt.plot(x, eta + Y_super, label="super")
 plt.plot(x, eta + Y_cr, label="Critical", color="black", linestyle="dashed", alpha=0.7)
 plt.plot(x, eta + Y_u, label="Uniform flow", color="black", alpha=0.7)
 plt.xlim([np.min(x), np.max(x)])
